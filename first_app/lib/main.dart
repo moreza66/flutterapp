b4c187d226eba484b553f903dcf3434829354cc2
mohammadreza memar <moreza.memar@gmail.com>
2023-07-03 13:05:18 -0500

    re run app

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,36 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(body: GradientContainer()),
-    ),
-  );
-}
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(body: GradientContainer()),
+//     ),
+//   );
+// }
 
 class GradientContainer extends StatelessWidget {
-
-  
-//   @override
-//   Widget build(context) {
-//     return Container(
-//       decoration: const BoxDecoration(
-//         gradient: LinearGradient(
-//           colors: [
-//             Color.fromARGB(255, 171, 144, 233),
-//             Color.fromARGB(255, 26, 2, 80),
-//           ],
-//           begin: Alignment.topLeft,
-//           end: Alignment.bottomCenter,
-//         ),
-//       ),
-//       child: const Center(
-//         child: Text('Hello World!',
-//             style: TextStyle(
-//                 fontSize: 48,
-//                 color: Colors.white,
-//                 fontWeight: FontWeight.bold)),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 171, 144, 233),
+            Color.fromARGB(255, 26, 2, 80),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello World!',
+            style: TextStyle(
+                fontSize: 48,
+                color: Colors.white,
+                fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
